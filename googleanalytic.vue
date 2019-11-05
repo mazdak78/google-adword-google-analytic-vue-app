89a1a43b4cea49d58d63513c4631d93087b72d81
Mazdak Mansouri <mazdak78@yahoo.com>
2019-11-05 08:06:31 -0500

    Create googleanalytic.vue

---
+++ googleanalytic.vue
@@ -0,0 +1,25 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "googleAnalytic",
+        mounted(){
+
+            // just set a title and page
+            var title =  window.location.pathname;
+            var page = location.href;
+
+            //ID from Google Analytic tracking code
+            gtag('config', 'UA-XXXXXXXX-X', {
+                'page_title' : page,
+                'page_path': title
+            });
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
